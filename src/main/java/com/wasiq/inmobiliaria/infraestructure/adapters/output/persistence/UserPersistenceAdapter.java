a324a9dba07b64bfd9ca816db339f34e4cce8c5c
Axel <dsg4857@gmail.com>
2026-01-16 00:09:32 -0500

    add: UserRepositoryPort, UserEntity, UserJpaRepository for Ports

---
+++ src/main/java/com/wasiq/inmobiliaria/infraestructure/adapters/output/persistence/UserPersistenceAdapter.java
@@ -0,0 +1,4 @@
+package com.wasiq.inmobiliaria.infraestructure.adapters.output.persistence;
+
+public class UserPersistenceAdapter {
+}
---
+++ src/main/java/com/wasiq/inmobiliaria/infraestructure/adapters/output/persistence/entity/UserEntity.java
@@ -0,0 +1,38 @@
+package com.wasiq.inmobiliaria.infraestructure.adapters.output.persistence.entity;
+
+import com.wasiq.inmobiliaria.domain.model.user.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255,nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "google_id")
+    private String googleId;
+
+    @Column(name = "full_name", length = 255)
+    private String fullName;
+
+    private String pictureUrl;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private boolean active;
+    private LocalDateTime createdAt;
+
+}
