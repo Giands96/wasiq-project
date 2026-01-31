6acd961252d779449bfa6a436a9c5b984eafa112
Axel <dsg4857@gmail.com>
2026-01-31 18:00:05 -0500

    add: Anotacion PostMapping para testing de Controller

--- src/main/java/com/wasiq/inmobiliaria/auth/controller/AuthController.java
+++ src/main/java/com/wasiq/inmobiliaria/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.wasiq.inmobiliaria.auth.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @PostMapping("/test")
     public String testAuth() {
         return "Auth Controller is working!";
     }
