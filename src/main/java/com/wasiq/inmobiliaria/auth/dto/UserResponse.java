ef7a1378b229260bcf5ed0ce575da334e7fd362d
Axel <dsg4857@gmail.com>
2026-02-23 22:57:05 -0500

    feat: Añadir campo id a UserResponse

--- src/main/java/com/wasiq/inmobiliaria/auth/dto/UserResponse.java
+++ src/main/java/com/wasiq/inmobiliaria/auth/dto/UserResponse.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserResponse {
 
+    private Long id;
     private String firstName;
     private String lastName;
     private String email;
