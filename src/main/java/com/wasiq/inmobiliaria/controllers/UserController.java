6135486edffa6bcfce8c82b6214460efd0d44354
Axel <dsg4857@gmail.com>
2026-03-23 15:25:29 -0500

    feat: Añadir autorización para endpoints de usuario y agregar AuthService

--- src/main/java/com/wasiq/inmobiliaria/controllers/UserController.java
+++ src/main/java/com/wasiq/inmobiliaria/controllers/UserController.java
@@ -1,5 +1,8 @@
 package com.wasiq.inmobiliaria.controllers;
 
+import com.wasiq.inmobiliaria.auth.dto.AuthResponse;
+import com.wasiq.inmobiliaria.auth.dto.UpdateUserRequest;
+import com.wasiq.inmobiliaria.auth.service.AuthService;
 import com.wasiq.inmobiliaria.models.Role;
 import com.wasiq.inmobiliaria.models.User;
 import com.wasiq.inmobiliaria.services.UserService;
@@ -7,19 +10,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
-
     private final UserService userService;
+    private final AuthService authService;
 
     @GetMapping("/by-role")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<Page<User>> findByRole(Pageable pageable, Role role){
         return ResponseEntity.ok().body(userService.findByRole(
                 role,
@@ -28,7 +32,11 @@ public class UserController {
         ));
     }
 
+
+
+
     @GetMapping("/all-desc")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<Page<User>> findAllByOrderDesc(Pageable pageable){
         return ResponseEntity.ok().body(userService.findAllByOrderByIdDesc(
                 pageable.getPageNumber(),
