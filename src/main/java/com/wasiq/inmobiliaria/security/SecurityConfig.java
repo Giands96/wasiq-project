33217b403a8e8ceadc61de7c88cc37139ab8d403
Axel <dsg4857@gmail.com>
2026-05-11 11:46:56 -0500

    feat: Update CORS configuration to allow all headers

--- src/main/java/com/wasiq/inmobiliaria/security/SecurityConfig.java
+++ src/main/java/com/wasiq/inmobiliaria/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://wasiq-project-front.vercel.app")); // Origen del Frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
